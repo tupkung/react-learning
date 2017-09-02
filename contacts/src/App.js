cf86b77d013f665106068fc8e7f82d6e0fb5f62a
Pheerawit Wasinphongwanit <pheerawit@gmail.com>
2017-09-02 21:34:44 +0700

    Finishing the Contact Form

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -22,17 +22,32 @@ class App extends Component {
     })
   }
 
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([contact])
+      }))
+    })
+  }
+
   render() {  
     return (
       <div className="app">
-        <Route exact path="/"  render={() => (
+        <Route exact path="/" render={() => (
           <ListContacts 
             onDeleteContact={this.removeContact} 
             contacts={this.state.contacts}
           />
         )}/>
 
-        <Route path="/create" component={CreateContact} />
+        <Route path="/create" render={( {history} ) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )} />
         
         
       </div>
